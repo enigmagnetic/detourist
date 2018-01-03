5f18d7b898cad22c7e7f0a8e177ed5faa241f147
enigmagnetic <lauren.cardella@gmail.com>
2018-01-03 14:48:59 -0800

    Initialize XCode project

---
+++ Detourist/Detourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Detourist
+//
+//  Created by Lauren Cardella on 1/3/18.
+//  Copyright © 2018 Lauren Cardella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
